4db76a457171411136bae87a9f43452dd2f20014
Simão Pedro Mendes Gabriel <spedromg@hotmail.com>
2023-02-19 11:21:28 -0200

    v19022023 - version basic battleship game

--- scripts/default.js
+++ scripts/default.js
@@ -3,28 +3,39 @@ function startBattheship() {
   var location1 = randomicLoc;
   var location2 = location1 + 1;
   var location3 = location2 + 1;
-  var guess;
+  var optionNumber; // insert number
   var hits = 0;
+  var errors = 0;
+  var isSunk = false; // flag
   var guesses = 0;
-  var isSunk = false;
 
   while (isSunk == false) {
-    guess = prompt('Ready, aim, fire! (enter a number from 0 a 6):');
+    guesses = guesses + 1;
+    optionNumber = prompt('Ready, aim, fire! (enter a number from 0 a 6):');
 
-    if (guess < 0 || guess > 6) {
+    if (optionNumber < 0 || optionNumber > 6) {
       alert('Please, enter a valid cell number!');
     } else {
-      guesses = guesses + 1;
-
-      if (guess == location1 || guess == location2 || location3) {
+      if (
+        optionNumber == location1 ||
+        optionNumber == location2 ||
+        optionNumber == location3
+      ) {
         alert('HIT!!!');
         hits = hits + 1;
+
         if (hits == 3) {
           isSunk = true;
           alert('You sank my battleship.');
         }
       } else {
         alert('MISS!!!');
+        errors = errors + 1;
+
+        if (errors == 3) {
+          alert('You lost!!!');
+          isSunk = true;
+        }
       }
     }
   }
@@ -34,5 +45,8 @@ function startBattheship() {
     guesses +
     ' guesses to sink the battleship, ' +
     'which means your shooting accuracy was ' +
-    3 / guesses;
+    ((hits / guesses) * 100).toFixed(2) +
+    '%';
+
+  alert(stats);
 }
